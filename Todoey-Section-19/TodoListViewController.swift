e52649db172a747dde27677f24f3d21b62bf81e1
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-30 15:21:55 +0300

    236-Creating a textfield inside UIAlert to add new items - UIAlertController, UIAlertAction, UITextField, breakpoint, reloadData, alert.action, alert.addTextField

--- Todoey-Section-19/TodoListViewController.swift
+++ Todoey-Section-19/TodoListViewController.swift
@@ -9,17 +9,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Hello", "Hi", "Hello again"]
-
+    var itemArray = ["Hello", "Hi", "Hello again"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
     
     // MARK: - Table view data source methods
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         itemArray.count
     }
@@ -44,6 +44,30 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
+    }
+    
+    // MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            // what will happen once the user clicks the Add Item button on our UIAlert
+            self.itemArray.append(textField.text!)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
     }
     
 }
