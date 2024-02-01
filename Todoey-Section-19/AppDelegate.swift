e019102c7a2b75af62dfa2b754698225e5434f8e
Abdurahman AYDIN <hariysu@gmail.com>
2024-02-01 18:25:44 +0300

    249-How to Set up and Configure Core Data - entity(table) = class, attributes = properties, current product module, class definition, category/extension, manual/none, lazy, NSManagedObjects, NSPersistentContainer: databases, SQL, SQLite, saveContext

--- Todoey-Section-19/AppDelegate.swift
+++ Todoey-Section-19/AppDelegate.swift
@@ -6,32 +6,55 @@
 //
 
 import UIKit
-
-@main
+import CoreData
+ 
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+ 
+    var window: UIWindow?
+ 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        // print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+ 
+ 
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+        self.saveContext()
+ 
     }
+ 
+    
+    // MARK: - Core Data stack
+    // We create a new NSPersistentContainer using our CoreData DataModel. And this persistentContainer is the database that we're going to be saving to and as a SQLITE database.
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    lazy var persistentContainer: NSPersistentContainer = {
+ 
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+ 
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+ 
+    // MARK: - Core Data Saving support
+    // We have this thing called a context which is what's called a scratch pad or a temporary area where you can update, change, delete your data until you're happy with the format. And then we basically commit it to permanent storage.
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+ 
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
-
-
+ 
 }
 
